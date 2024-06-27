979bf1751184085dcd10904e8f69af1f35f459db
Igor Galindo <igor.galindo.paiva@gmail.com>
2024-06-27 13:43:03 -0300

    ajuste função maker.ENuloOuVazio

--- pkg/maker/logica.go
+++ pkg/maker/logica.go
@@ -11,13 +11,25 @@ func ENuloOuVazio(valor interface{}) bool {
 		return true
 	}
 
-	switch v := reflect.ValueOf(valor); v.Kind() {
+	v := reflect.ValueOf(valor)
+
+	switch v.Kind() {
 	case reflect.String:
 		return strings.TrimSpace(v.String()) == ""
-	case reflect.Map, reflect.Slice:
+	case reflect.Map, reflect.Slice, reflect.Array:
 		return v.Len() == 0
 	case reflect.Struct:
 		return reflect.DeepEqual(valor, reflect.Zero(reflect.TypeOf(valor)).Interface())
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
 	default:
 		return false
 	}
